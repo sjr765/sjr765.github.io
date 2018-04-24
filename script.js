d6d06675cb76174b8bb976e05b3c0ff2a94b5f35
Sean Ryan <sjr765@gmail.com>
2018-04-23 22:12:03 -0400

    finished hovers on all images, added bio page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    $('.hover-text').hide();
-    $('.hover-container-left').hide();
+    $('.hover-text-one').hide();
+    $('.hover-container-one').hide();
     $('.picone').animate({
         opacity:1
         
@@ -8,13 +8,238 @@ $(document).ready(function() {
 
     $('.picone').hover(function() {
         $(this).stop().animate({opacity:.4},200);
-        $('.hover-text').fadeIn();
-        $('.hover-container-left').fadeIn();
+        $('.hover-text-one').fadeIn();
+        $('.hover-container-one').fadeIn();
+        
+    
 
     }, function() {
         $('.picone').stop().animate({opacity:1},500)
-        $('.hover-text').fadeOut();
-        $('.hover-container-left').fadeOut();
+        $('.hover-text-one').fadeOut();
+        $('.hover-container-one').fadeOut();
+
+    });
+    
+    $('.hover-text-one').hover(function() {
+        $('.picone').stop().animate({opacity:.4},200);
+        $(this).fadeIn();
+        $('.hover-container-one').fadeIn();
+        
+    
+
+    }, function() {
+        $('.picone').stop().animate({opacity:1},500)
+        $('.hover-text-one').fadeOut();
+        $('.hover-container-one').fadeOut();
+
+    });
+
+
+});
+
+
+//SECOND HOVER
+
+$(document).ready(function() {
+    $('.hover-text-two').hide();
+    $('.hover-container-two').hide();
+    $('.pictwo').animate({
+        opacity:1
+        
+    });
+
+    $('.pictwo').hover(function() {
+        $(this).stop().animate({opacity:.4},200);
+        $('.hover-text-two').fadeIn();
+        $('.hover-container-two').fadeIn();
+        
+    
+
+    }, function() {
+        $('.pictwo').stop().animate({opacity:1},500)
+        $('.hover-text-two').fadeOut();
+        $('.hover-container-two').fadeOut();
+
+    });
+    
+    
+    
+    $('.hover-text-two').hover(function() {
+        $('.pictwo').stop().animate({opacity:.4},200);
+        $(this).fadeIn();
+        $('.hover-container-two').fadeIn();
+        
+    
+
+    }, function() {
+        $('.pictwo').stop().animate({opacity:1},500)
+        $('.hover-text-two').fadeOut();
+        $('.hover-container-two').fadeOut();
+
+    });
+
+
+});
+
+//THIRD HOVER
+
+
+$(document).ready(function() {
+    $('.hover-text-three').hide();
+    $('.hover-container-three').hide();
+    $('.picthree').animate({
+        opacity:1
+        
+    });
+
+    $('.picthree').hover(function() {
+        $(this).stop().animate({opacity:.4},200);
+        $('.hover-text-three').fadeIn();
+        $('.hover-container-three').fadeIn();
+        
+    
+
+    }, function() {
+        $('.picthree').stop().animate({opacity:1},500)
+        $('.hover-text-three').fadeOut();
+        $('.hover-container-three').fadeOut();
+
+    });
+    
+    $('.hover-text-three').hover(function() {
+        $('.picthree').stop().animate({opacity:.4},200);
+        $(this).fadeIn();
+        $('.hover-container-three').fadeIn();
+        
+    
+
+    }, function() {
+        $('.picthree').stop().animate({opacity:1},500)
+        $('.hover-text-three').fadeOut();
+        $('.hover-container-three').fadeOut();
+
+    });
+
+
+});
+
+// Fourth HOVER
+
+$(document).ready(function() {
+    $('.hover-text-four').hide();
+    $('.hover-container-four').hide();
+    $('.picfour').animate({
+        opacity:1
+        
+    });
+
+    $('.picfour').hover(function() {
+        $(this).stop().animate({opacity:.4},200);
+        $('.hover-text-four').fadeIn();
+        $('.hover-container-four').fadeIn();
+        
+    
+
+    }, function() {
+        $('.picfour').stop().animate({opacity:1},500)
+        $('.hover-text-four').fadeOut();
+        $('.hover-container-four').fadeOut();
+
+    });
+    
+    $('.hover-text-four').hover(function() {
+        $('.picfour').stop().animate({opacity:.4},200);
+        $(this).fadeIn();
+        $('.hover-container-four').fadeIn();
+        
+    
+
+    }, function() {
+        $('.picfour').stop().animate({opacity:1},500)
+        $('.hover-text-four').fadeOut();
+        $('.hover-container-four').fadeOut();
+
+    });
+
+
+});
+
+// Fifth HOVER
+
+$(document).ready(function() {
+    $('.hover-text-five').hide();
+    $('.hover-container-five').hide();
+    $('.picfive').animate({
+        opacity:1
+        
+    });
+
+    $('.picfive').hover(function() {
+        $(this).stop().animate({opacity:.4},200);
+        $('.hover-text-five').fadeIn();
+        $('.hover-container-five').fadeIn();
+        
+    
+
+    }, function() {
+        $('.picfive').stop().animate({opacity:1},500)
+        $('.hover-text-five').fadeOut();
+        $('.hover-container-five').fadeOut();
+
+    });
+    
+    $('.hover-text-five').hover(function() {
+        $('.picfive').stop().animate({opacity:.4},200);
+        $(this).fadeIn();
+        $('.hover-container-five').fadeIn();
+        
+    
+
+    }, function() {
+        $('.picfive').stop().animate({opacity:1},500)
+        $('.hover-text-five').fadeOut();
+        $('.hover-container-five').fadeOut();
+
+    });
+
+
+});
+
+// Sixth HOVER
+
+$(document).ready(function() {
+    $('.hover-text-six').hide();
+    $('.hover-container-six').hide();
+    $('.picsix').animate({
+        opacity:1
+        
+    });
+
+    $('.picsix').hover(function() {
+        $(this).stop().animate({opacity:.4},200);
+        $('.hover-text-six').fadeIn();
+        $('.hover-container-six').fadeIn();
+        
+    
+
+    }, function() {
+        $('.picsix').stop().animate({opacity:1},500)
+        $('.hover-text-six').fadeOut();
+        $('.hover-container-six').fadeOut();
+
+    });
+    
+    $('.hover-text-six').hover(function() {
+        $('.picsix').stop().animate({opacity:.4},200);
+        $(this).fadeIn();
+        $('.hover-container-six').fadeIn();
+        
+    
+
+    }, function() {
+        $('.picsix').stop().animate({opacity:1},500)
+        $('.hover-text-six').fadeOut();
+        $('.hover-container-six').fadeOut();
 
     });
 
